dfbf96812e93f98c32927eba9da1e3c92d05e5fa
codehhr <1871973389@qq.com>
2020-11-28 16:07:17 +0800

    _

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,21 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        randomNum: "",
+        guessNum: ""
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    // 生成随机数
+    createRandNum() {
+        // 1 - 100
+        let randomNum = Math.floor(Math.random() * 100 + 1)
+        this.setData({
+            randomNum: randomNum
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 数据双向绑定( 猜的数 )
+    guessNum(e) {
+        this.setData({
+            guessNum: e.detail.value
+        })
     }
 })
