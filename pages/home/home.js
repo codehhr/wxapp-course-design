82adddb2dff1e7b588f6c507fef6b0f04f6bc37e
codehhr <1871973389@qq.com>
2020-11-28 20:53:19 +0800

    _

--- pages/home/home.js
+++ pages/home/home.js
@@ -16,8 +16,9 @@ Page({
         this.setData({
             randomNum: randomNum
         })
+        console.log("新的随机数: " + this.data.randomNum)
     },
-    // 数据双向绑定( 猜的数 )
+    // 数据双向绑定 ( 猜的数 input )
     guessNum(e) {
         this.setData({
             guessNum: e.detail.value
@@ -25,31 +26,52 @@ Page({
     },
     // 点击确定事件
     sure() {
-        if (this.data.guessNum == "") {
+        if (this.data.randomNum == "") {
             wx.showToast({
-                title: '请输入数字',
-                icon: "none"
+                title: '请先点击生成随机数',
+                icon: "none",
+                duration: 2000
             })
-            return;
-        }
-        if (/^([1-9]|[1-9]\d|100)$/.test(this.data.guessNum)) {
-            this.tellNum()
         } else {
-            wx.showToast({
-                title: '请输入1-100的正整数',
-                icon: "none"
-            })
+            if (this.data.guessNum == "") {
+                wx.showToast({
+                    title: '请输入数字',
+                    icon: "none"
+                })
+                return;
+            }
+            if (/^([1-9]|[1-9]\d|100)$/.test(this.data.guessNum)) {
+                this.tellNum()
+            } else {
+                wx.showToast({
+                    title: '请输入1-100的正整数',
+                    icon: "none"
+                })
+            }
         }
     },
     // 判断猜的数偏大或偏小
     tellNum() {
-        this.setData({
-            correct: true
-        })
+        if (this.data.guessNum > this.data.randomNum) {
+            wx.showToast({
+                title: '换个小点的数试试',
+                icon: "none",
+                duration: 2000
+            })
+        } else if (this.data.guessNum < this.data.randomNum) {
+            wx.showToast({
+                title: '换个大点的数试试',
+                icon: "none",
+                duration: 2000
+            })
+        } else {
+            this.setData({
+                correct: true
+            })
+        }
     },
-    //
+    // 关闭回答正确后的提示框
     closeHint() {
-        console.log("666")
         this.setData({
             correct: false
         })
