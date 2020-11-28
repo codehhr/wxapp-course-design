a10589d4778e42166757c288cd302604b402f5d7
codehhr <1871973389@qq.com>
2020-11-29 01:50:31 +0800

    _

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,6 +1,21 @@
 //logs.js
 
 Page({
-  data: {},
-
+  data: {
+    recordList: []
+  },
+  // onShow() 每次打开页面都会调用一次
+  onShow: function (options) {
+    this.setData({
+      recordList: wx.getStorageSync('record')
+    })
+    console.log(this.data.recordList)
+  },
+  // 清空记录
+  cleanRcord() {
+    wx.setStorageSync('record', [])
+    this.setData({
+      record: wx.getStorageSync('record')
+    })
+  }
 })
